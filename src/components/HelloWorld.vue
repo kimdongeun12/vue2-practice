6ac456a3c04959a64c8df993773cf7580e34122c
김동은 <kdedong12@172.20.nate.com>
2023-02-01 19:03:37 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,76 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<div class="date-picker-wrap">
+  <v-date-picker
+    v-model="dates"
+    range
+    locale="kr"
+    width="308"
+    :no-title="true"
+    :show-current="false"
+    :allowed-dates="disablePastDates"
+    min="2023-02-12"
+    max="2023-04-11"
+  ></v-date-picker>
+  <v-date-picker
+    v-model="dates"
+    range
+    :no-title="true"
+    width="308"
+    :allowed-dates="disablePastDates"
+    min="2023-02-12"
+    max="2023-04-11"
+  ></v-date-picker>
+
+</div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+      <script>
+  export default {
+    data: () => ({
+      dates: ['' , ''],
+    }),
+    computed: {
+      dateRangeText () {
+        return this.dates.join(' ~ ')
+      },
+    },
+    methods: {
+      disablePastDates(val) {
+        const newDate = new Date()
+        const prevDate = newDate.toISOString().substr(0, 10);
+        // const date = new Date(val).getDay();
+        return val >= prevDate
+      },
+    }
   }
-}
 </script>
+<style lang="scss" scoped>
+  .date-picker-wrap:deep {
+    .v-date-picker-table table{
+      border-spacing: 0 4px;
+    }
+     .v-date-picker-table .v-btn.v-btn--active {
+      color: red;
+    }
+    .v-date-picker-table--date .v-btn{
+      display: block;
+      width: 100%;
+      height: auto;
+      padding-top: 25%;
+      padding-bottom: 25%;
+    }
+    .v-btn:before{
+      border-radius: 0;
+    }
+    .v-date-picker-table tr {
+      border-radius: 20px;
+    }
+    .v-date-picker--last-in-range:before{
+      border-radius: 0 20px 20px 0;
+    }
+    .v-date-picker--first-in-range:before{
+      border-radius: 20px 0 0 20px;
+    }
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  }
 </style>
